f2f7fd4102cd28025e585cb11f4531d5a47e9de0
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-10 21:58:38 +0530

    works commited

---
+++ Generics Sample/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Generics_Sample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Generics_Sample.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Branch> Branch { get; set; }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
